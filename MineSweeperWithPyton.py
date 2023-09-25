bd49ed38a6db05c1f99c7d78dc27352a08195c7b
Nacali Lozano <nakaly@gmail.com>
2023-09-25 16:53:48 +0100

    Start setBoard

--- MineSweeperWithPyton.py
+++ MineSweeperWithPyton.py
@@ -3,28 +3,37 @@ from random import randint
 
 class Cell_t:
     def __init__(self):
-        value = 0
-        visible = False
+        self.value = 0
+        self.visible = False
  
 class Board_t:
     def __init__(self, rows, columns, mines):
-        self = []
+        self.rows = rows
+        self.columns = columns
+        self.mines = mines
+        self.board = []
         for row in range(rows):
-            self.append
+            self.board.append([])
             for column in range(columns):
-                self[row][column] = Cell_t()
+                self.board[row].append(Cell_t())
         #Set mines
         for mine in range(mines):
-            row = randint() % rows
-            column = randint() % columns
-            while self[row][column].value == 9:
-                row = randint() % rows
-                column = randint() % columns
-            self[row][column].value = 9
+            row = randint(0, rows -1)
+            column = randint(0, columns - 1)
+            while self.board[row][column].value == 9:
+                row = randint(0, rows -1)
+                column = randint(0, columns - 1)
+            self.board[row][column].value = 9
+    def __len__(self):
+        return len(self.board)
             
 def setBoard():
     Board = Board_t(14, 18, 40)
-    print(Board)
+    for row in range(Board.rows):
+        for column in range(Board.columns):
+            if Board.board[row][column].value == 9:
+                if row - 1 in range(Board.rows) and column -1 in range(Board.columns):
+                    Board.board[row - 1][column - 1].value += 1
 def pushCell(self):
     pass
 def youLose():
