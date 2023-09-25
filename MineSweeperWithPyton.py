bbee83f11626c84d5634153cc1bb355a9bfe623e
Nacali Lozano <nakaly@gmail.com>
2023-09-25 14:50:32 +0100

    Modified Board_t class

--- MineSweeperWithPyton.py
+++ MineSweeperWithPyton.py
@@ -8,7 +8,9 @@ class Cell_t:
  
 class Board_t:
     def __init__(self, rows, columns, mines):
+        self = []
         for row in range(rows):
+            self.append
             for column in range(columns):
                 self[row][column] = Cell_t()
         #Set mines
@@ -20,8 +22,9 @@ class Board_t:
                 column = randint() % columns
             self[row][column].value = 9
             
-def SetBoard(self):
-    pass
+def setBoard():
+    Board = Board_t(14, 18, 40)
+    print(Board)
 def pushCell(self):
     pass
 def youLose():
@@ -29,6 +32,7 @@ def youLose():
 def youWin(self):
     pass
 if __name__ == "__main__":
+    setBoard()
     window = tkinter.Tk()
     
     
@@ -43,3 +47,4 @@ if __name__ == "__main__":
     button11.grid(row = 1, column = 1)
     
     window.mainloop()
+    
