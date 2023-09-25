b32268f52f228a9f112f36644dfc0fdf60074415
Nacali Lozano <nakaly@gmail.com>
2023-09-25 12:05:26 +0100

    First steps with tkinter

--- MineSweeperWithPyton.py
+++ MineSweeperWithPyton.py
@@ -1,9 +1,18 @@
-def Board(columns, rows):
+import tkinter
 
+def Board(rows, columns):
+    pass
 def SetBoard(self):
-    
+    pass
 def pushCell(self):
-    
+    pass
 def youLose():
-    
+    pass
 def youWin(self):
+    pass
+if __name__ == "__main__":
+    window = tkinter.Tk()
+    button1 = tkinter.Button(window, text = "Push", command = lambda: print("Hello World"))
+    button1.pack()
+    
+    window.mainloop()
