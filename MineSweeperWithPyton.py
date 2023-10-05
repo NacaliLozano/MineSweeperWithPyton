7023b4373a5adb60c9e7511606d8aad86aa664ab
Nacali Lozano <nakaly@gmail.com>
2023-10-05 18:49:32 +0100

    Add youWin method Add youLose method Change behaviour of pushCell

--- MineSweeperWithPyton.py
+++ MineSweeperWithPyton.py
@@ -1,4 +1,6 @@
+
 import tkinter
+from tkinter import messagebox
 from random import randint
 
 class Cell_t:
@@ -13,6 +15,9 @@ class Cell_t:
  
 class Board_t:
     def __init__(self, rows, columns, mines):
+        """Initializes an empty board and sets the mines"""
+        if mines >= rows * columns:
+            return
         self.rows = rows
         self.columns = columns
         self.mines = mines
@@ -31,7 +36,6 @@ class Board_t:
                 row = randint(0, rows -1)
                 column = randint(0, columns - 1)
             self.cells[row][column].value = 9
-
             
     def setBoard(self):
         """Sets the values of the cells with a mine nearby"""
@@ -58,33 +62,42 @@ class Board_t:
     
     def pushCell(self, row, column):
         """Reveals the cell and neighbour cells"""
-        if self.cells[row][column].visible == False:
-            self.cells[row][column].visible = True
-            self.cells[row][column].button.destroy()
-            self.cells[row][column].button = None
-            self.cells[row][column].label = tkinter.Label(window, text=str(self.cells[row][column].value))
-            self.cells[row][column].label.grid(row = row, column = column)
-            if self.cells[row][column].value >= 9:
-                self.youLose()
-                return
-            else:
-                if self.cells[row][column].value == 0:
-                    if row - 1 in range(self.rows) and column - 1 in range(self.columns):
-                        self.pushCell(row - 1, column - 1)
-                    if row - 1 in range(self.rows):
-                        self.pushCell(row - 1, column)
-                    if row - 1 in range(self.rows) and column + 1 in range(self.columns):
-                        self.pushCell(row - 1, column + 1)
-                    if column - 1 in range(self.columns):
-                        self.pushCell(row, column - 1)
-                    if column + 1 in range(self.columns):
-                        self.pushCell(row, column + 1)
-                    if row + 1 in range(self.rows) and column - 1 in range(self.columns):
-                        self.pushCell(row + 1, column - 1)
-                    if row + 1 in range(self.rows):
-                        self.pushCell(row + 1, column)
-                    if row + 1 in range(self.rows) and column + 1 in range(self.columns):
-                        self.pushCell(row + 1, column + 1)
+        #Return early if the cell is already visible
+        if self.cells[row][column].visible:
+            return
+        #Return early if stepped on a mine
+        if self.cells[row][column].value >= 9:
+            self.youLose()
+            return
+        self.cells[row][column].visible = True
+        #Return early if player won
+        if self.youWin():
+            self.showAllCells()
+            tkinter.messagebox.showinfo("Game over","You Win!!!")
+            return
+        #Turn the button into a label
+        self.cells[row][column].button.destroy()
+        self.cells[row][column].button = None
+        self.cells[row][column].label = tkinter.Label(window, text=str(self.cells[row][column].value))
+        self.cells[row][column].label.grid(row = row, column = column)
+        #Trigger neighbour cells
+        if self.cells[row][column].value == 0:
+            if row - 1 in range(self.rows) and column - 1 in range(self.columns):
+                self.pushCell(row - 1, column - 1)
+            if row - 1 in range(self.rows):
+                self.pushCell(row - 1, column)
+            if row - 1 in range(self.rows) and column + 1 in range(self.columns):
+                self.pushCell(row - 1, column + 1)
+            if column - 1 in range(self.columns):
+                self.pushCell(row, column - 1)
+            if column + 1 in range(self.columns):
+                self.pushCell(row, column + 1)
+            if row + 1 in range(self.rows) and column - 1 in range(self.columns):
+                self.pushCell(row + 1, column - 1)
+            if row + 1 in range(self.rows):
+                self.pushCell(row + 1, column)
+            if row + 1 in range(self.rows) and column + 1 in range(self.columns):
+                self.pushCell(row + 1, column + 1)
                 
     def rightClick(self, row, column):
         self.cells[row][column].button.configure(bg="red")
@@ -95,16 +108,37 @@ class Board_t:
         self.cells[row][column].button.bind('<Button-1>', lambda event, row = row, column = column: self.pushCell(row, column))
         self.cells[row][column].button.bind('<Button-3>', lambda event, row = row, column = column: self.rightClick(row, column))
         self.cells[row][column].button.configure(bg="SystemButtonFace")
+
+    def showAllCells(self):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                if self.cells[row][column].button == None:
+                    continue
+                self.cells[row][column].button.destroy()
+                self.cells[row][column].button = None
+                self.cells[row][column].label = tkinter.Label(window, text=str(self.cells[row][column].value))
+                self.cells[row][column].label.grid(row = row, column = column)
+
     def youLose(self):
-        pass
+        self.showAllCells()
+        messagebox.showinfo("Game over","You lost!!!")
+        
     def youWin(self):
-        pass
+        notVisible = 0
+        for row in range(self.rows):
+            for column in range(self.columns):
+                if not self.cells[row][column].visible:
+                    notVisible += 1
+        print(notVisible)
+        if notVisible == self.mines:
+            return True
+        else:
+            return False
+
 if __name__ == "__main__":
-    
     window = tkinter.Tk()
     Board = Board_t(14, 18, 40)
     Board.setBoard()
-    
     #button11 = tkinter.Label(window, text="1")
     window.mainloop()
     
